630f1f3c21ba0cdf9d797c7fa501086e065c1c53
Damon Romano <rromano@creditacceptance.com>
2024-10-04 11:18:18 -0400

    fetching book data in Books.jsx; displaying book titles

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function Books() {
+  const [bookData, setBookData] = useState([]);
+
+  useEffect(() => {
+    axios(`${import.meta.env.VITE_API_BASE_URL}/api/books`)
+      .then((data) => {
+        console.log(data.data.books);
+        setBookData(data.data.books);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
-    <div>Books</div>
-  )
+    <div>
+      {bookData.map((book) => (
+        <p key={book.id}>{book.title}</p>
+      ))}
+    </div>
+  );
 }
 
-export default Books
+export default Books;
