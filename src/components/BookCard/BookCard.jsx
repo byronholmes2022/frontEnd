29232969fb6752e1be1c21bbf13148fabe769e6e
Damon Romano <rromano@creditacceptance.com>
2024-10-08 12:11:55 -0400

    fixed a syntax error in booklist.jsx

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 function BookCard({ book }) {
     return (
-        <Link to={`/books/${book.id}`} className="book-card">
-            <h3 >{book.title}</h3>
-            <img src={book.coverimage} alt={book.title} />
-        </Link>
+        <>
+            <Link to={`/books/${book.id}`} className="book-card anchor-base-style">
+                <h3 >{book.title}</h3>
+                <img src={book.coverimage} alt={book.title} />
+            </Link>
+        </>
     );
 }
 
