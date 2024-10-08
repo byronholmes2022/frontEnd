b77838b20a344fddc97294d5c80c4327b484ccc4
Damon Romano <rromano@creditacceptance.com>
2024-10-08 10:42:53 -0400

    Completesd functionality for SingleBook component - fetches data for specified book and displays that data.

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useParams } from "react-router-dom";
+import axios from 'axios';
 
 function SingleBook() {
+  const { bookid } = useParams();
+  const [book, setBook] = useState(null);
+
+  useEffect(() => {
+    axios(`${import.meta.env.VITE_API_BASE_URL}/api/books/${bookid}`)
+      .then((data) => {
+        console.log(data.data.book);
+        setBook(data.data.book);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+
   return (
-    <div>SingleBook</div>
+    <div>
+      <h2>{book.title}</h2>
+    </div>
   )
 }
 
