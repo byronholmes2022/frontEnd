8c7ba6a81c60e2c0acbfd517b377ee4e370e4293
Damon Romano <rromano@creditacceptance.com>
2024-10-15 10:48:53 -0400

    addede Login.jsx functionality

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function Login() {
+
+  const [token, setToken] = useState(null);
+  const [loginData, setLoginData] = useState({});
+
+  const handleUserInput = (e) => {
+    setLoginData({ ...loginData, [e.target.name]: e.target.value });
+  };
+
+  const handleLoginSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const data = await axios.post(
+        `${import.meta.env.VITE_API_BASE_URL}/api/users/login`,
+        loginData
+      );
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  console.log(loginData);
   return (
-    <div>Login</div>
-  )
-}
+    <div className='register-container'>
+      <h2>Login</h2>
+      <form className='register-form' onSubmit={handleLoginSubmit}>
+        <label>
+          <p>Email</p>
+          <input type='email' name='email' onChange={handleUserInput} />
+        </label>
+        <label>
+          <p>Password</p>
+          <input type="password" />
+        </label>
+        <button>Login</button>
+      </form>
+    </div>
+  );
+
+};
 
 export default Login
