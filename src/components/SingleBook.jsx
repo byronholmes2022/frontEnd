2979b118cfd9fbf8a0eaf8a312c7459c749c73aa
Damon Romano <rromano@creditacceptance.com>
2024-10-18 10:13:31 -0400

    added conditional rendering for login and for whether a particular book is checked out on SingleBook page

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 
-function SingleBook() {
+function SingleBook({ token }) {
+  const navigate = useNavigate();
   const { bookid } = useParams();
   const [book, setBook] = useState(null);
 
@@ -23,6 +24,10 @@ function SingleBook() {
         src={book?.coverimage}
         alt={`Cover image for ${book?.title} by ${book?.author}`}
       />
+      <p>{book?.description}</p>
+      {token && book?.available && <button>Check Out Book</button>}
+      {token && !book?.available && <p>Book Already Checked Out</p>}
+      {!token && <button onClick={() => Navigate("/login")}>Login</button>}
     </div>
   );
 }
